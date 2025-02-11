7803436d6b65c0433a6593ac8491d5fdcfd079e4
Isabel Morell <202207975@alu.comillas.edu>
2025-02-11 15:32:11 +0100

    medidas de clasificacion completadas

--- src/utils.py
+++ src/utils.py
@@ -90,6 +90,19 @@ def evaluate_classification(predictions: torch.Tensor, labels: torch.Tensor) -> 
     Returns:
         dict: A dictionary containing the calculated metrics.
     """
-    metrics: Dict[str, float] = None
+    metrics: Dict[str, float] = {}
+
+    positives = (labels == 1)
+    negatives = (labels == 0)
+
+    tp = (predictions[predictions == 1] == positives[predictions == 1]).sum()
+    fp = (predictions[predictions == 1] != positives[predictions == 1]).sum()
+    tn = (predictions[predictions == 0] == negatives[predictions == 0]).sum()
+    fn = (predictions[predictions == 0] != negatives[predictions == 0]).sum()
+
+    metrics["accuracy"] = (tp + tn)/(tp + tn + fp + fn)
+    metrics["precision"] = tp/(tp + fp)
+    metrics["recall"] = tp/(tp + fn)
+    metrics["f1_score"] = 2*tp/(2*tp + fp + fn)
 
     return metrics
